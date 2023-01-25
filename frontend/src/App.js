8d5397ee90c63b01bebc8ccf8cab3dea14b78e89
mastershashank <beingshashank08@gmail.com>
2023-01-25 10:10:51 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,74 @@
+import abi from "./contracts/comp.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Reg from "./components/Reg";
+import Memos from "./components/Memos";
+import thana from "./thana.jpg";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("None");
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x983cf2BaA93925e5c8C412C48BBa62Bd0f03e70D";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+          );
+          setAccount(account);
+          setState({ provider, signer, contract });
+        } else {
+          alert("Please install metamask");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+  // console.log(state);
+  return (
+    <div style={{ backgroundColor: "#EFEFEF", height: "40%" }}>
+      <div class="center">
+        <img src={thana} className="img-fluid" alt=".." width="40%" />
+      </div>
+      <p
+        class="text-muted lead "
+        style={{ marginTop: "10px", marginLeft: "5px" }}
+      >
+        <small>Connected Account - {account}</small>
+      </p>
+      <div className="container">
+        <Reg state={state} />
+        <Memos state={state} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
